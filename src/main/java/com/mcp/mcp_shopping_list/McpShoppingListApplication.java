e7505b0307aa79fba9c3a1ad1b702ba952d70081
Abhijeet Muneshwar <OPENINGKNOTS@GMAIL.COM>
2025-07-17 12:30:35 +0530

    ⚡ First commit
    Introduced Git Ignore and README

---
+++ src/main/java/com/mcp/mcp_shopping_list/McpShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.mcp_shopping_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpShoppingListApplication.class, args);
+	}
+
+}
